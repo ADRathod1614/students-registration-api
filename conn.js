14dee33de9720e7a55e42fbd2856fa77d9fd535b
ADRathod1614 <125894378+ADRathod1614@users.noreply.github.com>
2023-04-23 14:36:26 +0530

    Add files via upload

---
+++ conn.js
@@ -0,0 +1,9 @@
+const mongoose=require('mongoose');
+mongoose.connect("mongodb://127.0.0.1:27017/students-api",{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(()=>{
+    console.log("connection is done")
+}).catch((err)=>{
+    console.log(err)
+})
